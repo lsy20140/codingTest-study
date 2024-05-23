978bab6387b01d4fe776aa565ed0ee5127574305
lsy20140 <68267094+lsy20140@users.noreply.github.com>
2024-05-23 16:18:29 +0900

    [level 3] Title: 등굣길, Time: 4.95 ms, Memory: 35.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
+++ JavaScript/프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,23 +1,31 @@
-function solution(m, n, puddles) {
-  const MOD = 1_000_000_007;
-  // n행 m열 배열 생성
-  const dp = Array.from({ length: n }, () => Array(m).fill(0));
+function isPuddle(m, n, puddles){
+    for(let k = 0 ; k < puddles.length ; k++){
+        if(puddles[k][0]-1 === m && puddles[k][1]-1 === n){
+          return true
+        }
+    }
+  return false
+}
 
-  dp[0][0] = 1;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (i === 0 && j === 0) continue;
-        // 인덱스 값을 기준으로 보기에 사실상 x === j, y === i의 좌표와 같은 맥락
-        // 물 웅덩이에 빠진 경우 해당 경로 값 0으로 초기화
-      if (puddles.some(([x, y]) => x === j + 1 && y === i + 1)) {
-        dp[i][j] = 0;
-      } else {
-        // x, y이동에 필요한 걸음 % MOD(문제에서 제시)
-        dp[i][j] = ((i > 0 ? dp[i - 1][j] : 0) + (j > 0 ? dp[i][j - 1] : 0)) % MOD;
+function solution(m, n, puddles) {
+  let dp = Array.from(Array(n), () => Array(m).fill(0))
+  const MOD = 1_000_000_007
+  
+  dp[0][0] = 1
+  for(let i = 0 ; i < n ; i++) {
+    for(let j = 0 ; j < m ; j++){
+      if(isPuddle(j, i, puddles)) continue
+      if(i === 0 && j === 0) continue
+
+      if(i === 0){
+        dp[i][j] = dp[i][j-1]
+      }else if(j === 0){
+        dp[i][j] = dp[i-1][j]
+      }else{
+        dp[i][j] = ((i > 0 ? dp[i-1][j] : 0) + (j > 0 ? dp[i][j-1] : 0)) % MOD
       }
     }
   }
-
-    // 우측 하단까지 필요한 경로 수 반환
-  return dp[n - 1][m - 1];
+  return dp[n-1][m-1] 
 }
+
