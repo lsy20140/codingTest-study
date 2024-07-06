76c8f6cb90234446093a72d846af982c539b18f1
lsy20140 <68267094+lsy20140@users.noreply.github.com>
2024-07-06 12:15:07 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,18 @@
+function solution(ingredient) {
+    let answer = 0
+    let stack = []
+    
+    for(let i = 0 ; i < ingredient.length ; i++){
+        stack.push(ingredient[i])
+        
+        if(stack.length >= 4){
+            const sliced = stack.slice(-4).join("")
+            
+            if(sliced === "1231"){
+                stack.splice(-4)
+                answer++
+            }
+        }
+    }
+    return answer
+}
